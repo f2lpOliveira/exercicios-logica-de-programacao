e5815926c171fe3fb51a595d3b6438c26fc6ca7a
f2lpOliveira <contato@fellipefreire.com.br>
2023-01-30 06:12:57 -0300

    Exercício 1

---
+++ ex01/operacoesAritimeticas.js
@@ -0,0 +1,20 @@
+// EXERCÍCIO 1
+// Faça as 4 operações básicas com base nos números 2 e 4:
+// Ao final de cada operação imprina natela no seguinte formato:
+// Soma: 2 + 4 = x
+// Subtração: 2 - 4 = x
+// Divisão: 2 / 4 = x
+// Multiplicação: 2 * 4 = x
+
+let num1 = 2;
+let num2 = 4;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2;
+
+console.log('Soma: ' + num1 + ' + ' + num2 + ' = ' + soma);
+console.log('Subtração: ' + num1 + ' + ' + num2 + ' = ' + subtracao);
+console.log('Divisão: ' + num1 + ' + ' + num2 + ' = ' + divisao);
+console.log('Multiplicação: ' + num1 + ' + ' + num2 + ' = ' + multiplicacao);
