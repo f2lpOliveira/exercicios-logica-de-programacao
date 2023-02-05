bfb1668ada91641002bad97e6c4c9565b6bc99ab
f2lpOliveira <contato@fellipefreire.com.br>
2023-02-05 17:52:57 -0300

    Refatoração Exercício 9

--- ex09/stairCase.js
+++ ex09/stairCase.js
@@ -7,14 +7,12 @@
 // Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
 // Write a program that prints a staircase of size n.
 
-let start = 4;
-
-function stairCase() {
+function stairCase(n) {
   let value = " ";
   let char = "#";
-  for (let i = 1; i <= start; i++) {
-    console.log(value.repeat(start - i) + char.repeat(i));
+  for (let i = 1; i <= n; i++) {
+    console.log(value.repeat(n - i) + char.repeat(i));
   }
 }
 
-stairCase();
+stairCase(10);
