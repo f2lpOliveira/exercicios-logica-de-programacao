a0879d424b172018533c3f2c99abbccd45371828
f2lpOliveira <contato@fellipefreire.com.br>
2023-02-13 06:41:59 -0300

    Refatoração Exercício 7

--- ex07/aVeryBigSum.js
+++ ex07/aVeryBigSum.js
@@ -1,13 +1,12 @@
 // In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 
-let sum = [2, 3, 4, 5, 6];
+function aVeryBigSum(arr) {
+  let sum = 0;
 
-function aVeryBigSum(sum) {
-  let total = 0;
-  for (let i = 0; i < sum.length; i++) {
-    total += sum[i];
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
-  return total;
+  return sum;
 }
 
-console.log(aVeryBigSum(sum));
+console.log(aVeryBigSum([2, 3, 4, 5, 6]));
