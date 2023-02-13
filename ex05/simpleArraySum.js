607d052b722a4090f97e8fb7139ad6c50c39dff0
f2lpOliveira <contato@fellipefreire.com.br>
2023-02-13 06:40:54 -0300

    Refatoração Exercício 5

--- ex05/simpleArraySum.js
+++ ex05/simpleArraySum.js
@@ -1,9 +1,9 @@
 // Given an array of integers, find the sum of its elements.
 // For example, if the array ar = [1, 2, 3], 1 + 2 + 3 = 6, so return 6.
 
-let ar = [1, 2, 3];
-
 function simpleArraySum(ar) {
+  let ar = [1, 2, 3];
+
   let soma = 0;
   for (i = 0; i < ar.length; i++) {
     soma += ar[i];
