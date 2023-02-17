3aaed6e4584d0bd91f7d72cbc319742c0662a667
f2lpOliveira <contato@fellipefreire.com.br>
2023-02-17 17:12:52 -0300

    Refatoração Exercício 10

--- ex10/miniMaxSum.js
+++ ex10/miniMaxSum.js
@@ -4,18 +4,25 @@
 // The minimum sum is 1 + 3 + 5 + 7 = 16 and the maximum sum is 3 + 5 + 7 + 9 = 24. The function prints 16 24.
 
 function miniMaxSum(arr) {
-  let miniSum = 0;
-  let maxSum = 0;
+  let menorNumero = Math.min(...arr);
+  let maiorNumero = Math.max(...arr);
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < arr[i + 1]) {
-      miniSum += arr[i];
-      if (arr[arr.length - 1] > arr[arr.length - 2]) {
-        maxSum += arr[arr.length - [i + 1]];
-      }
-    }
-  }
-  console.log(miniSum, maxSum);
+  let somaArrSemMenorNumero = 0;
+  let somaArrSemMaiorNumero = 0;
+
+  let positionMinSum = arr.indexOf(menorNumero);
+  let positionMaxSum = arr.indexOf(maiorNumero);
+
+  let arrSemMenorNumero = arr.slice();
+  let arrSemMaiorNumero = arr.slice();
+
+  arrSemMenorNumero.splice(positionMinSum, 1);
+  arrSemMaiorNumero.splice(positionMaxSum, 1);
+
+  arrSemMenorNumero.forEach(numero => (somaArrSemMenorNumero += numero));
+  arrSemMaiorNumero.forEach(numero => (somaArrSemMaiorNumero += numero));
+
+  return console.log(somaArrSemMaiorNumero, somaArrSemMenorNumero);
 }
 
-miniMaxSum([1, 2, 3, 4, 5]);
+miniMaxSum([1, 3, 5, 7, 9]);
